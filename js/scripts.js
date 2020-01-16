42da8356197f904db2725bbae9697d7dfa04d190
Student <example@example.com>
2020-01-15 16:26:14 -0800

    center align text.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,12 @@
 $(document).ready(function(){
-$( "#target" ).click(function() {
-  alert( "why did you click that?" );
-});
+// $( "#target" ).click(function() {
+//   alert( "why did you click that?" );
+  var quest = (confirm("Will you take our survey?"));
+
+  if (quest) {
+    alert('please continue');
+  } else {
+    alert('get out');
+  }
+event.preventDefault();
 });
